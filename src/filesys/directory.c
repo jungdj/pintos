f318f17d0d6dd8c4598d112ae586f8a19e6befd4
jungdj <jung_dj@kaist.ac.kr>
2019-12-16 19:22:37 +0900

    [Project4] Fix missing parts

--- src/filesys/directory.c
+++ src/filesys/directory.c
@@ -216,7 +216,7 @@ dir_lookup (const struct dir *dir, const char *name,
    Fails if NAME is invalid (i.e. too long) or a disk or memory
    error occurs. */
 bool
-dir_add (struct dir *dir, const char *name, block_sector_t inode_sector)
+dir_add (struct dir *dir, const char *name, block_sector_t inode_sector, bool is_dir)
 {
   struct dir_entry e;
   off_t ofs;
